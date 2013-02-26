c9fb713d89b17dc7e03d73e05fa0177e72d88c93
unknown <mmorn@PBB-C320R-14.iowa.uiowa.edu>
2013-02-25 20:40:03 -0600

    initial commit

---
+++ Index.aspx
@@ -0,0 +1,45 @@
+﻿<%@ Page Language="VB" AutoEventWireup="false" CodeFile="Index.aspx.vb" Inherits="Index" %>
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+    
+        Net Pay Calculator
+    
+    </div>
+    <p>
+        Hourly Wage:</p>
+    <p>
+        <asp:TextBox ID="hourlywageTextBox" runat="server"></asp:TextBox>
+    </p>
+    <p>
+        Number of Hours Worked This Week:</p>
+    <p>
+        <asp:TextBox ID="hoursWorkedTextBox" runat="server"></asp:TextBox>
+    </p>
+    <p>
+        Pre-Tax Deductions:</p>
+    <p>
+        <asp:TextBox ID="pretaxTextBox" runat="server"></asp:TextBox>
+    </p>
+    <p>
+        After-Tax Deductions:
+    </p>
+    <p>
+        <asp:TextBox ID="aftertaxTextBox" runat="server"></asp:TextBox>
+    </p>
+    <p>
+        <asp:Button ID="calcButton" runat="server" Text="Calculate Me!!" />
+&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+        <asp:Button ID="clearButton" runat="server" Text="Clear Me!!" />
+    </p>
+    <asp:Label ID="netpayLabel" runat="server"></asp:Label>
+    </form>
+</body>
+</html>
---
+++ Index.aspx.vb
@@ -0,0 +1,55 @@
+﻿
+Partial Class Index
+    Inherits System.Web.UI.Page
+
+    Protected Sub Page_Load(sender As Object, e As System.EventArgs) Handles Me.Load
+
+    End Sub
+    Function Calculate() As Decimal
+        Const taxRate As Decimal = 0.18
+
+        'declare variables!!
+        Dim wage As Decimal = Val(hourlywageTextBox.Text)
+        Dim hours As Decimal = Val(hoursWorkedTextBox.Text)
+        Dim pretax As Decimal = Val(pretaxTextBox.Text)
+        Dim after As Decimal = Val(aftertaxTextBox.Text)
+
+        'calculate gross earnings/week
+        Dim grossEarnings As Decimal
+        grossEarnings = wage * hours
+
+
+        'Calculate after pretax deduction
+        Dim pretaxResult As Decimal
+        pretaxResult = grossEarnings - pretax
+
+        'Calculate tax after pretax deductions
+        Dim totalTax As Decimal
+        totalTax = pretaxResult * taxRate
+
+        'calculate net pay
+        Dim netpay As Decimal
+        netpay = pretaxResult - totalTax - after
+
+        'return net pay
+        Return netpay
+
+    End Function
+    Protected Sub calcButton_Click(sender As Object, e As System.EventArgs) Handles calcButton.Click
+        'display net pay in label in dollar currency format
+        netpayLabel.Text = String.Format("{0:C}", Calculate())
+
+    End Sub
+
+    Protected Sub clearButton_Click(sender As Object, e As System.EventArgs) Handles clearButton.Click
+        'clear textboxes
+
+        hourlywageTextBox.Text = ""
+        hoursWorkedTextBox.Text = ""
+        pretaxTextBox.Text = ""
+        aftertaxTextBox.Text = ""
+        netpayLabel.Text = ""
+
+
+    End Sub
+End Class
